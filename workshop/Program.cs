1c047706bca9f41dc5cd4e4884b14277fb11971e
oskar-t-fredriksson <72459215+oskar-t-fredriksson@users.noreply.github.com>
2021-02-24 16:05:35 +0100

    Pretty much fully functional blackjack for 1 player

--- workshop/Program.cs
+++ workshop/Program.cs
@@ -23,7 +23,12 @@ namespace workshop
             while (points > 0)
             {
                 card = Deck();
-                Console.WriteLine(card);
+                if (card == 1)
+                {
+                    Console.WriteLine("Do you want 1 or 14?");
+                    card = Convert.ToInt32(Console.ReadLine());
+                }
+                Console.WriteLine($"Player: {card}");
                 Console.WriteLine("How much do you want to deposit?");
                 deposit = Convert.ToInt32(Console.ReadLine());
                 points -= deposit;
@@ -35,21 +40,30 @@ namespace workshop
                     if (dScore < plScore)
                     {
                         Console.WriteLine("You won this round.");
-                        points += deposit * 2;
+                        points = points + (deposit * 2);
+                        Console.WriteLine($"Player points: {points}");
+                        Console.WriteLine($"Dealer points: {dpoints}");
                         continue;
                     }
                     Console.WriteLine("You lost this round.");
                     //Printa vad resultatet blev (deposit)
+                    dpoints += deposit;
+                    Console.WriteLine($"Player points: {points}");
+                    Console.WriteLine($"Dealer points: {dpoints}");
                 }
                 else if (plScore == 21){
                     Console.WriteLine("You won this round.");
-                    points += deposit * 2;
+                    points = points + (deposit * 2);
+                    Console.WriteLine($"Player points: {points}");
+                    Console.WriteLine($"Dealer points: {dpoints}");
                 }
                 else
                 {
                     Console.WriteLine("You lost this round.");
                     //printa vad givaren fick
                     dpoints += deposit;
+                    Console.WriteLine($"Player points: {points}");
+                    Console.WriteLine($"Dealer points: {dpoints}");
                 }
                 Console.WriteLine("Want to keep playing? Y/N");
                 //can add a tryparse here later
@@ -63,28 +77,63 @@ namespace workshop
             }
 
         }
-         static int Player(int card)
+        static int Player(int card)
         {
             int card1 = card;
             int sum = card1;
             string more = "y";
-            while (more == "y" || sum < 21)
+            while (more == "y" && sum < 21)
             {
                 //få ett kort
                 int nextcard = Deck();
+                if (nextcard == 1)
+                {
+                    Console.WriteLine("Do you want 1 or 14?");
+                    nextcard = Convert.ToInt32(Console.ReadLine());
+                }
                 sum += nextcard;
-                Console.WriteLine(sum);
+                Console.WriteLine($"Player: {sum}");
                 Console.WriteLine("Want another card? Y/N");
                 more = Console.ReadLine().ToLower();
             }
             return sum;
 
         }
-        static int Deck()
+        
+        //måste fixas så man inte kan ta mer än 4 av samma            
+        private static readonly Random random = new Random();
+        private static readonly object syncLock = new object();
+        static int Deck() {
+
+            lock (syncLock)
+            { // synchronize
+                return random.Next(1, 14);
+            }
+        }
+          
+        static int Dealer(int plScore)
         {
-            //måste fixas så man inte kan ta mer än 4 av samma
-            Random rnd = new Random();
-            return rnd.Next(1, 14);
+            int card = Deck();
+            int sum = card;
+            Console.WriteLine($"Dealer: {sum}");
+            while (sum < plScore)
+            {
+                int nextCard = Deck();
+                if(nextCard == 1)
+                {
+                    if(sum <= 7)
+                    {
+                        nextCard = 14;
+                    }                    
+                }
+                sum += nextCard;
+                Console.WriteLine($"Dealer: {sum}");
+            }
+            if(sum > 21)
+            {
+                sum = 0;
+            }
+            return sum;
         }
     }
 }
